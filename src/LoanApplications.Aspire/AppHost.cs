53d79e639bd0ce2fa2c933ad9ed4b69af64f96ce
bencorker <ben.corker@holidaygems.co.uk>
2026-03-22 11:00:56 +0000

    connect up aspire

--- src/LoanApplications.Aspire/AppHost.cs
+++ src/LoanApplications.Aspire/AppHost.cs
@@ -1,5 +1,7 @@
 using Aspire.Hosting;
+using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
-
+builder.AddProject<LoanApplications_API>("api").WithUrl("/api-docs", "Scalar API Docs");
+builder.AddProject<LoanApplications_BackgroundProcessor>("background-processor").WithExplicitStart();
 builder.Build().Run();
